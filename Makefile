75b882990470c3742370eb71a08c78815d349040
Aureane Moraes <aureane.moraes@outlook.com>
2023-04-20 12:13:41 -0300

    finalizad aula 02

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ migrate:
 migratedown:
 	migrate -path=sql/migrations -database "mysql://root:root@tcp(localhost:3307)/chat_test" -verbose drop	
 
-# grpc:
-# 	protoc --go_out=. --go-grpc_out=. proto/chat.proto
+grpc:
+	protoc --go_out=. --go-grpc_out=. proto/chat.proto
 
 .PHONY: migrate createmigration migratedown grpc
--- cmd/chatservice/main.go
+++ cmd/chatservice/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 
 	"github.com/aureanemoraes/chatservice/configs"
+	"github.com/aureanemoraes/chatservice/internal/infra/grpc/server"
 	"github.com/aureanemoraes/chatservice/internal/infra/repository"
 	"github.com/aureanemoraes/chatservice/internal/infra/web"
 	"github.com/aureanemoraes/chatservice/internal/infra/web/webserver"
 	"github.com/aureanemoraes/chatservice/internal/usecase/chatcompletion"
+	"github.com/aureanemoraes/chatservice/internal/usecase/chatcompletionstream"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/sashabaranov/go-openai"
 )
@@ -40,31 +42,31 @@ func main() {
 		InitialSystemMessage: configs.InitialChatMessage,
 	}
 
-	// chatConfigStream := chatcompletionstream.ChatCompletionConfigInputDTO{
-	// 	Model:                configs.Model,
-	// 	ModelMaxTokens:       configs.ModelMaxTokens,
-	// 	Temperature:          float32(configs.Temperature),
-	// 	TopP:                 float32(configs.TopP),
-	// 	N:                    configs.N,
-	// 	Stop:                 configs.Stop,
-	// 	MaxTokens:            configs.MaxTokens,
-	// 	InitialSystemMessage: configs.InitialChatMessage,
-	// }
+	chatConfigStream := chatcompletionstream.ChatCompletionConfigInputDTO{
+		Model:                configs.Model,
+		ModelMaxTokens:       configs.ModelMaxTokens,
+		Temperature:          float32(configs.Temperature),
+		TopP:                 float32(configs.TopP),
+		N:                    configs.N,
+		Stop:                 configs.Stop,
+		MaxTokens:            configs.MaxTokens,
+		InitialSystemMessage: configs.InitialChatMessage,
+	}
 
 	usecase := chatcompletion.NewChatCompletionUseCase(repo, client)
 
-	// streamChannel := make(chan chatcompletionstream.ChatCompletionOutputDTO)
-	// usecaseStream := chatcompletionstream.NewChatCompletionUseCase(repo, client, streamChannel)
+	streamChannel := make(chan chatcompletionstream.ChatCompletionOutputDTO)
+	usecaseStream := chatcompletionstream.NewChatCompletionUseCase(repo, client, streamChannel)
 
-	// fmt.Println("Starting gRPC server on port " + configs.GRPCServerPort)
-	// grpcServer := server.NewGRPCServer(
-	// 	*usecaseStream,
-	// 	chatConfigStream,
-	// 	configs.GRPCServerPort,
-	// 	configs.AuthToken,
-	// 	streamChannel,
-	// )
-	// go grpcServer.Start()
+	fmt.Println("Starting gRPC server on port " + configs.GRPCServerPort)
+	grpcServer := server.NewGRPCServer(
+		*usecaseStream,
+		chatConfigStream,
+		configs.GRPCServerPort,
+		configs.AuthToken,
+		streamChannel,
+	)
+	go grpcServer.Start()
 
 	webserver := webserver.NewWebServer(":" + configs.WebServerPort)
 	webserverChatHandler := web.NewWebChatGPTHandler(*usecase, chatConfig, configs.AuthToken)
